6a59167df4e21072cc51d868c16a0fd7fe3e249d
BiCheng Sha <BiChengSha@users.noreply.github.com>
2016-10-03 08:07:48 -0400

    Update Lab4-ViaPhone.c

--- Lab-4/Lab4-ViaPhone.c
+++ Lab-4/Lab4-ViaPhone.c
@@ -11,56 +11,48 @@ PRG155-NGH?
 
 
 int main(){
+  
+  //Question 1: if num > 0 then x3 and output result, if <0 then x(-1) and output abs val and if 0 then output =0
+  float numIn, numResult;
+
+  puts("Enter a number: ");
+  scanf("%f", &numIn);
+
+  if (numIn > 0){
+    numResult = numIn * 3;
+    printf("%f.2 multiplied by 3 is %f.2", numIn, numResult);
+  } else if (numIn < 0) {
+    numResult = numIn * (-1);
+    printf("The absolute value of %f.2 is %f.2", numIn, numResult);
+  } else if (numIn == 0){
+    puts("Your number is 0");
+  } else {
+    puts("Invalid number.");
+  }
 
 
-
-
-//Question 1: if num > 0 then x3 and output result, if <0 then x(-1) and output abs val and if 0 then output =0
-
-
-
-float numIn, numResult;
-
-puts("Enter a number: ");
-scanf("%f", &numIn);
-
-if (numIn > 0){
-numResult = numIn * 3;
-printf("%f.2 multiplied by 3 is %f.2", numIn, numResult);
-} else if (numIn < 0) {
-numResult = numIn * (-1);
-printf("The absolute value of %f.2 is %f.2", numIn, numResult);
-} else if (numIn == 0){
-puts("Your number is 0");
-} else {
-puts("Invalid number.");
-}
-
-
-///////////////////////////////////
-printf("\n\n\n"); //clear lines
+  ///////////////////////////////////
+  printf("\n\n\n"); //clear lines
 
 
 
 
 
+  //Question 2:
+  int firstNum, secondNum, product;
 
-//Question 2:
+  puts("Enter an integer: ");
+  scanf("%d", &firstNum);
+  puts("Enter a second integer: ");
+  scanf("%d", secondNum);
 
-
-int firstNum, secondNum, product;
-
-puts("Enter an integer: ");
-scanf("%d", &firstNum);
-puts("Enter a second integer: ");
-scanf("%d", secondNum);
-
-
-if(firstNum > 100 && secondNum > 100){
-
-} else if (firstNum > 100 || secondNum > 100){
-
-}
+  if(firstNum > 100 && secondNum > 100){
+    puts("Both numbers are greater than 100");
+    product = firstNum * secondNum;
+    printf("%d", product);
+  } else {
+    puts("At least one of the numbers are less than 100."); 
+  }
 
 
 
