ce5b3ebce5284f5a36ded45c5029133b432f8242
BiCheng Sha <BiChengSha@users.noreply.github.com>
2016-10-02 23:59:49 -0400

    Update Lab4-ViaPhone.c

--- Lab-4/Lab4-ViaPhone.c
+++ Lab-4/Lab4-ViaPhone.c
@@ -9,3 +9,23 @@ PRG155-NGH?
 #include<stdio.h>
 #include<math.h>
 
+
+int main(){
+
+
+
+
+//Question 1: if num > 0 then x3 and output result, if <0 then x(-1) and output abs val and if 0 then output =0
+
+
+
+float numIn;
+
+puts("Enter a number: ");
+scanf("%f");
+
+
+
+
+
+}
