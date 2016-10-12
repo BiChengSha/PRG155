0c59cd2d455ff2c51fd29bc2266570328c51101b
BiCheng Sha <BiChengSha@users.noreply.github.com>
2016-10-12 08:29:50 -0400

    Update Lab5.c

--- Lab-5/Lab5.c
+++ Lab-5/Lab5.c
@@ -7,3 +7,27 @@ Class: PRG155-NGH
 */
 
 
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+	
+	//Question 1: Calculate a) Parallel resistance, b) Current through a resistor, c) Exit program
+	
+	int choice, numResist;
+	float parallel[numResist], 
+	char resistUnit;
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
