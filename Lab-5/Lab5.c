e9bc560670ca8bf1cca35549d636dba274be796b
BiCheng Sha <BiChengSha@users.noreply.github.com>
2016-10-12 09:21:26 -0400

    Update Lab5.c

--- Lab-5/Lab5.c
+++ Lab-5/Lab5.c
@@ -18,14 +18,58 @@ int main() {
 	float resist1a, resist1b, totalPResist, volt, current;
 	char resistUnit1a, resistUnit1b, resistUnit2;
 	
+	
+	puts("Choose one of the 3 options below: ");
 	printf("\t\t\tMenu\n");
 	puts("1. Calculate Parallel Resistance.");
 	puts("2. Calculate current flowing through a resistor.");
 	puts("3. Exit.");
-	
+	printf("\n\nChoose either 1, 2, or 3: ");
+	scanf("%d", &choice);
 	
 	if(choice == 1){
 		puts("You have selected option 1 - Calculating parallel resistance.");
+		printf("\n\nEnter value of first resistor: ");
+		scanf("%f", &resist1a);
+		printf("Enter unit of measurement for the first resistor (n, u, m, O, K, M, G): ");
+		scanf("%f", &resistUnit1a);
+		printf("\n\nEnter value of second resistor: ");
+		scanf("%f", &resist1b);
+		printf("Enter unit of measurement for the second resistor (n, u, m, O, K, M, G): ");
+		scanf("%f", &resistUnit1b);
+		
+		if(resistUnit1a == 'n'){
+			resist1a = resist1a * pow(10, -9);
+		} else if (resistUnit1a == 'u'){
+			resist1a = resist1a * pow(10, -6);
+		} else if (resistUnit1a == 'm'){
+			resist1a = resist1a * pow(10, -3);
+		} else if (resistUnit1a == 'O'){
+			resist1a = resist1a * pow(10, 0);
+		} else if (resistUnit1a == 'K'){
+			resist1a = resist1a * pow(10, 3);
+		} else if (resistUnit1a == 'M'){
+			resist1a = resist1a * pow(10, 6);
+		} else if (resistUnit1a == 'G'){
+			resist1a = resist1a * pow(10, 9);
+		}
+		
+		if(resistUnit1b == 'n'){
+			resist1b = resist1b * pow(10, -9);
+		} else if (resistUnit1b == 'u'){
+			resist1b = resist1b * pow(10, -6);
+		} else if (resistUnit1b == 'm'){
+			resist1b = resist1b * pow(10, -3);
+		} else if (resistUnit1b == 'O'){
+			resist1b = resist1b * pow(10, 0);
+		} else if (resistUnit1b == 'K'){
+			resist1b = resist1b * pow(10, 3);
+		} else if (resistUnit1b == 'M'){
+			resist1b = resist1b * pow(10, 6);
+		} else if (resistUnit1b == 'G'){
+			resist1b = resist1b * pow(10, 9);
+		}
+		
 	} else if(choice == 2){
 		puts("You have selected option 2 - Calculating current flowing through a resistor.");
 	} else if(choice == 3){
