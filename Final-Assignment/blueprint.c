013fccee0b8d5a2f4ebef0965e5e2186aa08b8c5
BiCheng Sha <BiChengSha@users.noreply.github.com>
2016-11-12 21:15:47 -0500

    Update blueprint.c

--- Final-Assignment/blueprint.c
+++ Final-Assignment/blueprint.c
@@ -32,33 +32,6 @@ int main(){ //Main function to call up other functions and main logic
 }
 
 int getValidData(int low, int high, char type){ //Validating user inputs
-   
-   int userInput;
-   
-   
-   if(type == 'w'){  //checking for number of windows
-   
-      puts("Enter the number of windows: "); /////////////////////////////////////
-      scanf("%d", &userInput);               //////Getting number of windows//////
-      fflush(stdin);                         /////////////////////////////////////
-      
-      while(userInput >= low, userInput <= high){
-         puts("Invalid value for number of windows. Enter again: ");    //forcing user to re-enter if invalid
-         scanf("%d", &userInput); 
-         fflush(stdin); 
-      }
-   } else if (type == 'f'){   //checking for number of floors
-   
-      puts("Enter the number of floors: ");  ////////////////////////////////////
-      scanf("%d", &userInput);               //////Getting number of floors//////
-      fflush(stdin);                         ////////////////////////////////////
-      
-      while(userInput >= low, userInput <= high){
-         puts("Invalid value for number of floors. Enter again: ");     //forcing user to re-enter if invalid
-         scanf("%d", &userInput); 
-         fflush(stdin); 
-      }
-   }
    return(userInput);
 }
 
